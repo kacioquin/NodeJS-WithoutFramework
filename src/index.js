e60d2abe27718c6f6dc656f971fd28721c45f3bb
Cassio Quintino <cassio.quintino@gmail.com>
2022-03-22 10:11:37 -0300

    parsing url using URL native module

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 const http = require('http');
+const { URL } = require('url')
 
 const routes = require('./routes')
 
 const server = http.createServer((request, response) => {
-    console.log(`Request method: ${request.method} | Endpoint: ${request.url}`)
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`)
+  
+  console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`)
 
-    // const route = routes.find((routeObj) => (
-    //     routeObj.endpoint === request.url && routeObj.method === request.method
-    // ))
+  const route = routes.find((routeObj) => routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method )
 
-    const route = routes.find((routeObj) => routeObj.endpoint === request.url && routeObj.method === request.method )
-
-    if(route) {
-        route.handler(request, response)
-    } else {
-        response.writeHead(404, { 'Content-Type': 'text/html' })
-        response.end(`Cannot ${request.method} ${request.url}`)
-    }
+  if(route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams)
+    route.handler(request, response)
+  } else {
+      response.writeHead(404, { 'Content-Type': 'text/html' })
+      response.end(`Cannot ${request.method} ${request.url}`)
+  }
 })
 
 server.listen(3000, () => console.log('Server started at http://localhost:3000'))
